eeaeb8c4cf9f17c711514e2a961aaee6ebf969e2
Gabriel Carvalho <grpc15@outlook.com>
2023-05-15 11:21:11 -0300

    home - estilização finalizada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para sua tarefa" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para sua tarefa"
+          />
+          <datalist id="task-suggestions">
+            <option value="um" />
+            <option value="dois" />
+            <option value="tres" />
+            <option value="quatro" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -35,6 +35,7 @@ const BaseInput = styled.input`
   font-weight: bold;
   font-size: 1.125rem;
   padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
 
   &:focus {
     box-shadow: none;
@@ -48,6 +49,9 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
