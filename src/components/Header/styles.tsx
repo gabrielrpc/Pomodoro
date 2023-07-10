a0cb7c05726491648368f3012ff938a0e693ee67
Gabriel Carvalho <grpc15@outlook.com>
2023-07-10 08:21:11 -0300

    Estilo da home desktop finalizado

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -4,8 +4,6 @@ export const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 0.5rem;
-
   nav {
     display: flex;
     gap: 0.5rem;
--- src/layouts/DefaultLayout/styles.tsx
+++ src/layouts/DefaultLayout/styles.tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  max-height: 100vh;
-  margin: 4.5rem auto;
-  padding: 2rem 2.5rem;
+  min-height: calc(100vh - 10rem);
+  margin: 3.5rem auto;
+  padding: 2rem;
 
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
