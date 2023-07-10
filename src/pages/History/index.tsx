b9b5f27def6facedaa5f0c1aa3f2d75deb8d54b7
Gabriel Carvalho <grpc15@outlook.com>
2023-07-10 08:46:12 -0300

    Finalizado a estilização desktop do history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,60 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export const History = () => {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>Completa</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>Completa</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>Completa</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>Completa</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>Completa</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>Completa</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
